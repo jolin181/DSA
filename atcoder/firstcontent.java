7540326f32cb38003a17ace0d265b1ec3be0311f
JOLIN A <jolinprith442@gmail.com>
2026-01-24 18:11:51 +0530

    Modify logic for calculating result based on inputs

--- atcoder/firstcontent.java
+++ atcoder/firstcontent.java
@@ -2,9 +2,10 @@ import java.util.*;
 public class content {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        int f=sc.nextInt();
         int d=sc.nextInt();
-        int ans=d-(f%7);
-        System.out.println(ans==0?7:ans);
+        int f=sc.nextInt();
+        int ans=f-(d%7);
+        if(ans<=0)ans+=7;
+        System.out.println(ans);
     }
 }
