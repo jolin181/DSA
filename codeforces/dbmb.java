bc056d658f36d6d4ab19f69738b98d89918b49bb
JOLIN A <jolinprith442@gmail.com>
2026-01-25 20:41:27 +0530

    Add dbmb.java for problem solution

---
+++ codeforces/dbmb.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class dbmb{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int time=sc.nextInt();
+        while(time-->0){
+            int n=sc.nextInt();
+            int s=sc.nextInt();
+            int x=sc.nextInt();
+            int tot=0;
+            for(int i=0;i<n;i++){
+                tot+=sc.nextInt();
+            }
+           if(tot>s){
+               System.out.println("NO");
+               continue;
+           }
+           int sub=s-tot;
+           System.out.println(sub%x==0?"YES":"NO");
+        }
+    }
+}
