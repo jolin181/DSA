688ec46bef0f7b5549779a399fa633ac07a8e156
JOLIN A <jolinprith442@gmail.com>
2026-01-26 19:18:27 +0530

    Add blacksel.java for processing input and output

---
+++ codeforces/blacksel.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int times=sc.nextInt();
+        while(times-->0){
+            int k=sc.nextInt();
+            int x=sc.nextInt();
+            System.out.println((k*x)+1);
+        }
+
+    }
+}
