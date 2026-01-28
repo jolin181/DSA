57b5582454a8ed51be0e4b0f202ef93754ef2c06
JOLIN A <jolinprith442@gmail.com>
2026-01-28 18:12:27 +0530

    Add triangular number calculation in triangularnum.java

---
+++ atcoder/triangularnum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class triangular {
+    public static void main(String[] argsr){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double val=(n/2.0)+0.5;
+        System.out.println((int)(n*val));
+
+    }
+}
