312cc5236b31f21a820825880c55d363c0cbd46b
JOLIN A <jolinprith442@gmail.com>
2026-01-29 17:52:09 +0530

    Add solution to check for perfect numbers

---
+++ Leetcode/perfectnum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num<=1)return false;
+        int sum=1;
+        int div=2;
+        while((div*div)<=num){
+            if(num%div==0){
+                sum+=div;
+            
+            if(div!=num/div){
+                sum+=num/div;
+            }
+            }
+            div++; 
+        }
+        return sum==num;
+    }
+}
