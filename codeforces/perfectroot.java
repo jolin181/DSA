c90f89489a4f407ffd4c33fc17a55a1e0bb81620
JOLIN A <jolinprith442@gmail.com>
2026-02-01 18:27:53 +0530

    Add perfectroot.java to print numbers up to n

---
+++ codeforces/perfectroot.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class perfectroot {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int times=sc.nextInt();
+        while(times-->0){
+            int n= sc.nextInt();
+            for(int i=1;i<=n;i++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
