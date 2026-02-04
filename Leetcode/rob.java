c992a7fdba016c9f914d0c76c73d10fe925130d9
JOLIN A <jolinprith442@gmail.com>
2026-02-04 10:49:19 +0530

    Update rob.java

--- Leetcode/rob.java
+++ Leetcode/rob.java
@@ -2,12 +2,14 @@ class Solution {
     public int rob(int[] nums) {
         if(nums.length<2)return nums[0];
         int []arr=new int[nums.length];
-        arr[0]=nums[0];
-        arr[1]=nums[0]>nums[1]?nums[0]:nums[1];
+        int p1=nums[0];
+        int p2=nums[0]>nums[1]?nums[0]:nums[1];
         for(int i=2;i<nums.length;i++){
-            arr[i]=Math.max(arr[i-2]+nums[i],arr[i-1]);
+            int tem=Math.max(p1+nums[i],p2);
+            p1=p2;
+            p2=tem;
         }
-        return arr[nums.length-1];
+        return p2;
     
     }
 }
