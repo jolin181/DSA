6016cf95a9479aacb501fa6b14bde5e86fc724f5
JOLIN A <jolinprith442@gmail.com>
2026-02-05 14:35:16 +0530

    Create octave.java

---
+++ atcoder/octave.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class octve {
+    public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);
+         int x= sc.nextInt();
+         int y= sc.nextInt();
+         System.out.println((int)(x*(Math.pow(2,y))));
+    }
+}
