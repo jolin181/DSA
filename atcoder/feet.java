274f0bb30e587c4d86ae1e8c64bed47ff57181d7
JOLIN A <jolinprith442@gmail.com>
2026-02-06 18:14:28 +0530

    Create feet.java

---
+++ atcoder/feet.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int f= sc.nextInt();
+        int in= sc.nextInt();
+        System.out.println((f*12)+in);
+    }
+}
